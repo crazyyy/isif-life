447836397eea88ebf047a5bc63eb0d3319e3fa55
crazyyy <crazyyy@gmail.com>
2016-08-02 09:53:03 +0300

    reinit project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,23 @@
-!function(){for(var e,o=function(){},n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],r=n.length,i=window.console=window.console||{};r--;)e=n[r],i[e]||(i[e]=o)}(),"undefined"==typeof jQuery?console.warn("jQuery hasn't loaded"):console.log("jQuery has loaded");
-//# sourceMappingURL=maps/scripts.js.map
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function() {};
+  var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+if (typeof jQuery === 'undefined') {
+  console.warn('jQuery hasn\'t loaded');
+} else {
+  console.log('jQuery has loaded');
+}
+// Place any jQuery/helper plugins in here.
